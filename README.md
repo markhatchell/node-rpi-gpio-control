1b53a9c25457d3f84914e781e6d7fb80cd4e1a34
Mark Hatchell <mark.hatchell@prospect33.com>
2017-02-26 12:48:43 -0500

    initial working commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# rpi-gpio-control
+
+Controls the raspberry pi GPIO via the file system.
---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+function rpiGPIOControl() {
+
+    let pinsOn = [];
+
+    this.modes = {
+        write: 'out',
+        read: 'in'
+    };
+
+    const paths = {
+        export: '/sys/class/gpio/export',
+        unexport: '/sys/class/gpio/unexport',
+        pin: '/sys/class/gpio/gpio',
+        direction: '/direction',
+        value: '/value'
+    };
+
+    this.main = function() {
+    };
+
+    this.setup = function(pin, mode, cb) {
+        fs.writeFile(paths.export, pin, function() {
+            pinsOn.push(pin);
+            fs.writeFile(paths.pin + pin + paths.direction, mode, cb);
+        });
+    };
+
+    this.teardown = function(pin, cb) {
+        fs.writeFile(paths.unexport, pin, function(){
+            pinsOn.splice(pinsOn.indexOf(pin), 1);
+            cb();
+        });
+    };
+
+
+    this.write = function(pin, value, cb) {
+        fs.writeFile(paths.pin + pin + paths.value, value, cb);
+    };
+
+    this.read = function(pin, cb) {
+        fs.readFile(paths.pin + pin + paths.value, cb);
+    };
+}
+
+module.exports = new rpiGPIOControl;
